27d41fa10a97a0184067a110b5c0a73850fcd813
Amir Maghami <Maghami.A1987@gmail.com>
2024-08-24 10:52:07 +0330

    Update Auth.vue

--- layouts/Auth.vue
+++ layouts/Auth.vue
@@ -3,7 +3,7 @@
     <section
       class="w-full max-w-lg bg-white shadow border border-solid border-gray-50 p-4 rounded"
     >
-      <NuxtLink to="/" class="block w-max mx-auto">
+      <NuxtLink to="/" class="block w-max mx-auto mb-4">
         <img class="rounded-full" src="https://placehold.co/200x200" title="logo" alt="logo" />
       </NuxtLink>
       <slot />
