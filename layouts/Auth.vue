66545c3c92c3cfdd078378b21479f332230bab42
Amir Maghami <Maghami.A1987@gmail.com>
2024-08-24 10:50:09 +0330

    Update Auth.vue

--- layouts/Auth.vue
+++ layouts/Auth.vue
@@ -4,7 +4,7 @@
       class="w-full max-w-lg bg-white shadow border border-solid border-gray-50 p-4 rounded"
     >
       <NuxtLink to="/">
-        <img src="https://placehold.co/400x400" title="logo" alt="logo" />
+        <img class="rounded-full" src="https://placehold.co/400x400" title="logo" alt="logo" />
       </NuxtLink>
       <slot />
     </section>
