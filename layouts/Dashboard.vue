6b2c05354d2ba17239d3c4199940aafc44c7e832
Amir Maghami <Maghami.A1987@gmail.com>
2024-08-24 11:15:10 +0330

    Update Dashboard.vue

--- layouts/Dashboard.vue
+++ layouts/Dashboard.vue
@@ -2,14 +2,16 @@
   <div>
     <Navbar />
     <main>
-      <nav>
-        <ul>
-          <NuxtLink to="/dashboard/">dashboard</NuxtLink>
-          <NuxtLink to="/dashboard/courses">courses</NuxtLink>
-          <NuxtLink to="/dashboard/messages">messages</NuxtLink>
-          <NuxtLink to="/dashboard/progress">progress</NuxtLink>
-          <NuxtLink to="/dashboard/settings">settings</NuxtLink>
-        </ul>
+      <nav class="py-6">
+        <div class="container mx-auto px-4">
+          <ul class="flex gap-x-4 justify-center">
+            <NuxtLink class="text-gray" to="/dashboard/">dashboard</NuxtLink>
+            <NuxtLink class="text-gray" to="/dashboard/courses">courses</NuxtLink>
+            <NuxtLink class="text-gray" to="/dashboard/messages">messages</NuxtLink>
+            <NuxtLink class="text-gray" to="/dashboard/progress">progress</NuxtLink>
+            <NuxtLink class="text-gray" to="/dashboard/settings">settings</NuxtLink>
+          </ul>
+        </div>
       </nav>
       <slot />
     </main>
